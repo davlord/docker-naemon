3360f0903650e604aabf3f96a1cb987c28c654ec
vbueb <vbueb@jnesis.com>
2017-02-09 13:52:50 +0100

    add embedded smtp in container (exim) to send naemon email alerts

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ RUN yum -y update
 RUN yum -y install epel-release
 RUN yum -y localinstall https://labs.consol.de/repo/stable/rhel7/x86_64/labs-consol-stable.rhel7.noarch.rpm
 RUN yum -y install naemon
+RUN yum -y install mailx exim
 RUN yum -y install nagios-plugins nagios-plugins-all nagios-plugins-nrpe nrpe
 RUN yum -y install supervisor
+RUN sed -i -e 's/begin rewrite/begin rewrite\n.include \/etc\/exim\/rewrite.conf/g' /etc/exim/exim.conf
 RUN cp -r /etc/naemon/conf.d /etc/naemon/conf.d.default
 COPY naemon.ini /etc/supervisord.d/naemon.ini
 COPY docker-entrypoint.sh /
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,19 @@ set -e
 
 readonly NAEMON_CONF_DIR=/etc/naemon/conf.d
 
-# if config directory is empty copy default configuration files in it
+
+# define sender email address for exim
+if [ -z "$EMAIL_SENDER" ]
+then
+    echo >&2 "you need to specify EMAIL_SENDER environment variable"
+    exit 1
+else
+    echo "using sender email: $EMAIL_SENDER"
+    echo "*@* $EMAIL_SENDER Ffr" > /etc/exim/rewrite.conf
+fi
+
+
+# if naemon config directory is empty copy default configuration files in it
 if [ ! "$(ls -A $NAEMON_CONF_DIR)" ]
 then
 	echo "$NAEMON_CONF_DIR is empty, creating default naemon configuration files"
